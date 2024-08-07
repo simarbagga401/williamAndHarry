6718e482aa611b6f8dc89548f5c017a2b747b544
simarbagga <simarbagga401@gmail.com>
2024-08-07 21:35:46 +0530

    index page done

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+</script>
+
+<template>
+  <div
+    class="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px] grid-container"
+  >
+    <div class="flex items-center justify-center py-12">
+      <div class="mx-auto grid w-[350px] gap-6">
+        <div class="grid gap-2 text-center">
+          <h1 class="text-3xl font-bold">Join</h1>
+          <p class="text-balance text-muted-foreground">
+            Buy premium Quality Coats from us
+          </p>
+        </div>
+        <div class="grid gap-4">
+          <Button variant="outline" class="w-full"> Login with Google </Button>
+        </div>
+      </div>
+    </div>
+    <div class="bg-muted lg:block image-container">
+      <img
+        src="~/assets/images/placeholder.jpg"
+        alt="Image"
+        height="100vh"
+        class="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale image"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.image {
+  height: 100vh;
+}
+@media screen and (max-width: 1023px) {
+  .image {
+    height: 75.6vh;
+  }
+}
+</style>

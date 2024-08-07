c5a3475eca0ba5ee8c7abfa409bbf0de6a7a2090
simarbagga <simarbagga401@gmail.com>
2024-08-07 23:07:59 +0530

    sign in and signout made

--- app.vue
+++ app.vue
@@ -4,8 +4,20 @@
   </div>
 </template>
 
+<script setup lang="ts">
+const user = useCurrentUser();
+const router = useRouter();
+onMounted(() => {
+  watch(user, (currentUser, prevUser) => {
+    if (!currentUser && prevUser) {
+      return router.replace("/login");
+    }
+  });
+});
+</script>
+
 <style>
-body{
-overflow: hidden;
+body {
+  overflow: hidden;
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "nuxt-vuefire"],
   shadcn: {
     /**
      * Prefix for all the imported component
@@ -13,5 +14,20 @@ export default defineNuxtConfig({
      * @default "./components/ui"
      */
     componentDir: "./components/ui",
+  },
+  vuefire: {
+    auth: {
+      enabled: true,
+      sessionCookie:false,
+    },
+    config: {
+      apiKey: "AIzaSyDp3g2LHR97UdcsgMhAQNT9NK7KNibwOL0",
+      authDomain: "williamandharry-9288e.firebaseapp.com",
+      projectId: "williamandharry-9288e",
+      storageBucket: "williamandharry-9288e.appspot.com",
+      messagingSenderId: "346862769568",
+      appId: "1:346862769568:web:4374117114cfb7841040c5",
+      measurementId: "G-3KWSNNR1B2",
+    },
   },
 });

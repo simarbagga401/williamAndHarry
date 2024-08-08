02fdbc6733eb1610eea74907c9b3c91f1aa272b3
simarbagga <simarbagga401@gmail.com>
2024-08-08 19:34:50 +0530

    changes

---
+++ pages/aboutUs.vue
@@ -0,0 +1,44 @@
+<template>
+  <main class="p-10 flex flex-col items-center justify-evenly w-full h-screen">
+    <nav class="h-1/6 flex items-center justify-center">
+      <img src="~/assets/images/logo.png" alt="logo" class="logo" />
+      <h1 class="font-bold text-4xl">About Us</h1>
+    </nav>
+    <section class="w-full h-3/4">
+      <p>
+        Welcome to William & Harry, your premier destination for exceptional
+        men's formal suits in Mumbai. At William & Harry, we believe that
+        elegance and affordability should go hand in hand. Our mission is to
+        provide impeccably crafted suits that cater to both style and budget,
+        ensuring that every man can experience the sophistication of a
+        well-tailored garment without breaking the bank. Founded with a
+        commitment to quality and value, our brand merges timeless designs with
+        modern tailoring techniques. Each suit is meticulously crafted using
+        premium fabrics, guaranteeing a perfect fit and lasting comfort. Our
+        dedicated team of skilled artisans ensures that every detail meets the
+        highest standards, reflecting our unwavering dedication to excellence.
+        <br />
+        <br />
+        At William & Harry, we understand that a great suit is more than just
+        clothing—it's a statement of confidence and refinement. Whether you're
+        preparing for a special occasion or enhancing your professional
+        wardrobe, we are here to help you make a lasting impression with ease.
+        <br />
+        <br />
+        Explore our collection and discover why William & Harry is Mumbai's
+        trusted name in affordable luxury.
+      </p>
+    </section>
+  </main>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.logo {
+  width: 60px;
+  position: absolute;
+  top: 10px;
+  left: 10px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,29 +2,40 @@
   <main class="flex flex-col items-center h-screen w-screen">
     <nav class="w-full flex items-center justify-end">
       <img src="~/assets/images/logo.png" alt="logo" class="logo" />
-      <p>{{ user?.displayName }}</p>
-      <img :src="user?.photoURL ?? ''" class="w-10 ml-3 rounded-full" />
+      <NuxtLink class="p-4" to="/myOrder">My orders</NuxtLink>
+      <NuxtLink class="p-4" to="/aboutUs" >About us</NuxtLink>
       <Button @click="handleSignOut" class="m-4">sign out</Button>
     </nav>
     <section class="flex items-center w-full justify-center h-full px-10">
-      <Carousel class="relative w-full max-w-xl">
-    <CarouselContent>
-      <CarouselItem v-for="(_, index) in 5" :key="index">
-        <div class="p-1">
-          <Card>
-            <CardContent class="flex aspect-square items-center justify-center p-6">
-              <span class="text-4xl font-semibold">{{ index + 1 }}</span>
-            </CardContent>
-          </Card>
-        </div>
-      </CarouselItem>
-    </CarouselContent>
-    <!-- <CarouselPrevious />
-    <CarouselNext /> -->
-  </Carousel>
+      <Carousel class="relative w-full max-w-sm">
+        <CarouselContent>
+          <CarouselItem>
+            <div class="p-1">
+              <Card>
+                <CardContent
+                  class="flex aspect-square items-center justify-center p-6"
+                >
+                  <img :src="suit.coatImage" />
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+          <CarouselItem>
+            <div class="p-1">
+              <Card>
+                <CardContent
+                  class="flex aspect-square items-center justify-center p-6"
+                >
+                  <img :src="suit.pantImage" />
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </section>
-
-
   </main>
 </template>
 
@@ -50,7 +61,15 @@ import {
 const auth = useFirebaseAuth();
 const user = useCurrentUser();
 
-const suits = [{ name: 'White Suit', description: ' A classic blue jacket, often in shades like navy or royal blue. Coordinating trousers in the same shade of blue as the coat', coatImage: '~/assets/images/white-coat.jpeg', pantImage: '~/assets/images/white-pant.jpeg' }, { name: 'Blue Suit', description: ' A classic blue jacket, often in shades like navy or royal blue. Coordinating trousers in the same shade of blue as the coat', coatImage: '~/assets/images/white-coat.jpeg', pantImage: '~/assets/images/white-pant.jpeg' }]
+import whiteCoatImage from "~/assets/images/white-coat.jpeg";
+import whitePantImage from "~/assets/images/white-pant.jpeg";
+const suit = {
+  name: "White Suit",
+  description:
+    " A classic blue jacket, often in shades like navy or royal blue. Coordinating trousers in the same shade of blue as the coat",
+  coatImage: whiteCoatImage,
+  pantImage: whitePantImage,
+};
 
 const handleSignOut = () => {
   if (auth) signOut(auth);
@@ -64,5 +83,4 @@ const handleSignOut = () => {
   top: 10px;
   left: 10px;
 }
-
 </style>

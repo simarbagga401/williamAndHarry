6122bf88e406db1c067ccb9a9e290e8d8a36b101
simarbagga <simarbagga401@gmail.com>
2024-08-20 19:02:15 +0530

    minor change in myOrders.vue

--- pages/myOrders.vue
+++ pages/myOrders.vue
@@ -2,7 +2,10 @@
   <section class="flex flex-col items-center w-full h-screen">
     <h1 class="font-bold text-4xl p-10">My Orders</h1>
     <div class="flex items-center justify-evenly h-200px">
-      <p class="font-medium text-2xl p-10" v-if="myOrders == null || myOrders.orders.length == 0">
+      <p
+        class="font-medium text-2xl p-10"
+        v-if="myOrders == null || myOrders.orders.length == 0"
+      >
         No Orders to Track
       </p>
       <section v-else>
@@ -14,7 +17,10 @@
               </CardHeader>
               <CardContent>
                 <template v-for="(product, i) in order.products" :key="i">
-                  <p>{{ product.name }} x {{ product.quantity }} x {{ product.size }}</p>
+                  <p>
+                    {{ product.name }} x {{ product.quantity }} x
+                    {{ product.coatSize }} x {{ product.pantSize }}
+                  </p>
                   <p>₹{{ product.price }}</p>
                 </template>
               </CardContent>
@@ -33,7 +39,11 @@
           </div>
         </div>
       </section>
-      <Button @click="navigateTo('/')" v-if="myOrders == null || myOrders.orders.length == 0">Shop</Button>
+      <Button
+        @click="navigateTo('/')"
+        v-if="myOrders == null || myOrders.orders.length == 0"
+        >Shop</Button
+      >
     </div>
   </section>
 </template>
